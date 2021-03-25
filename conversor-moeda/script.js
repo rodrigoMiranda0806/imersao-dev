543bfa88b9343c1adc7f43676baa154786583522
rodrigomir <rodrigo.mir.miranda@gmail.com>
2021-03-25 19:35:11 -0300

    Adicionando projeto calculadora

---
+++ conversor-moeda/script.js
@@ -0,0 +1,14 @@
+var valorEmDolarTexto = prompt("qual é o valor em dolar que você quer converter")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarNumero * 5.50
+var valorEmRealFixado = valorEmReal.toFixed(2)
+                                          
+alert(valorEmRealFixado)
+
+//Revisão
+//variáveis var int - Float - string 
+//alert - parseInt - parseFloat - prompt
+//operações + somar * multiplicar 
+// comentario final 
